2cc40a20935c46a5eee0e44e111a5be9a3899cc2
priyalgabani <priyalgabani2004@gmail.com>
2024-02-01 23:57:04 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int a =10;
+    int b = 20;
+    float c = 3.26;
+    bool d = false;
+    
+    cout << "sum of a + b = " << a+c << endl;
+    cout << d ;
+
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    int a,b,c,d;
+    cout << "Enter the value of a : ";
+    cin >> a;
+    cout << "Enter the value of b : ";
+    cin >> b;
+    c = a + b;
+    d = a * b;
+    cout << "sum of the a + b : " << c << endl;
+    cout << "Multiplication of the a * b : " << d;
+    return 0;
+}
